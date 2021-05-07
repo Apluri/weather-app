a0c1fd3e89ec8fa5f08be73fc3a2b754acb298fd
Apluri <aleksishot2@gmail.com>
2021-05-07 14:40:36 +0300

    Edited app to use new api format

--- app/src/main/java/fi/tiko/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/fi/tiko/weatherapp/ui/MainActivity.kt
@@ -11,54 +11,60 @@ import android.widget.Toast
 import fi.tiko.weatherapp.R
 import fi.tiko.weatherapp.data.EnvironmentVariables
 import fi.tiko.weatherapp.data.Request
+import org.json.JSONArray
 import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-    var city = "Tampere" // change to mobile location
-    lateinit var address : TextView
-    lateinit var temperature : TextView
-    lateinit var updatedAt : TextView
-    lateinit var status : TextView
-    lateinit var sunrise : TextView
-    lateinit var sunset : TextView
+    private val apiKey = EnvironmentVariables().apiKey
+
+    // TODO: 07/05/2021 Changing city does not do anything anymore due new api call, implement conversion to lat and long
+    var city = "Nokia" // change to mobile location
+
+    // TODO: 07/05/2021 change to dynamically change based on location or by city given
+    var lat = 61.48
+    var lon = 23.50
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("Test1", "App launched!")
-        address = findViewById(R.id.address)
-        temperature = findViewById(R.id.temp)
-        updatedAt = findViewById(R.id.updated_at)
-        status = findViewById(R.id.status)
-        sunrise = findViewById(R.id.sunrise)
-        sunset = findViewById(R.id.sunset)
-
         updateUi()
 
     }
 
     private fun updateUi() {
         thread {
-            val jsonObj = Request("https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=${EnvironmentVariables().apiKey}").getJsonObj()
-            val updateTime : Long = jsonObj.getLong("dt")
-            val sys = jsonObj.getJSONObject("sys")
-            runOnUiThread {
-                Toast.makeText(this, "Request performed", Toast.LENGTH_SHORT).show()
-                address.text = city
-                temperature.text = jsonObj.getJSONObject("main").getString("temp")+"°C"
-                updatedAt.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(updateTime*1000))
-                status.text = jsonObj.getJSONArray("weather").getJSONObject(0).getString("description")
-                sunrise.text = "Sunrise: " + SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sys.getLong("sunrise")*1000))
-                sunset.text = "Sunset: " + SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sys.getLong("sunset")*1000))
-
-
-            }
+            val weatherInfo : JSONObject = Request("https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&units=metric&exclude=minutely,hourly,alerts&appid=$apiKey").getJsonObj()
+            val currentWeather : JSONObject = weatherInfo.getJSONObject("current")
+            val dailyWeather : JSONArray = weatherInfo.getJSONArray("daily")
+            updateCurrentWeatherView(currentWeather)
+            updateDailyWeatherView(dailyWeather)
         }
     }
 
+    private fun updateCurrentWeatherView(currentWeather : JSONObject) {
+        val temp = currentWeather.getString("temp")+"°C"
+        val updateTime = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(currentWeather.getLong("dt")*1000))
+        val weatherDescription = currentWeather.getJSONArray("weather").getJSONObject(0).getString("description")
+        val sunrise = "Sunrise: " + SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(currentWeather.getLong("sunrise")*1000))
+        val sunset = "Sunset: " + SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(currentWeather.getLong("sunset")*1000))
+        runOnUiThread {
+            Toast.makeText(this, "Request performed", Toast.LENGTH_SHORT).show()
+            findViewById<TextView>(R.id.address).text = city
+            findViewById<TextView>(R.id.temp).text = temp
+            findViewById<TextView>(R.id.updated_at).text = updateTime
+            findViewById<TextView>(R.id.status).text = weatherDescription
+            findViewById<TextView>(R.id.sunrise).text = sunrise
+            findViewById<TextView>(R.id.sunset).text = sunset
+        }
+    }
+
+    private fun updateDailyWeatherView(dailyWeather : JSONArray) {
+        // TODO: 07/05/2021 Implement this
+    }
     fun openSettings(view : View) {
         Log.d("Test1", "open settings")
         val intent = Intent(this, SettingsActivity::class.java).apply {
